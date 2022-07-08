5ceb8485e39a113e90d7170338129d12af4edea9
Vincent H <hoangvincent69@gmail.com>
2022-07-07 18:36:02 -0700

    Tile Random Hexadecimal Colors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// Hexadecimal color generator
 const hexaColor = () => {
-  let str = '0123456789abcdef'
+  let str = '0123456789ABCDEF'
   let color = ''
   let hexArray = []
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 32; i++) {
     for(let j = 0; j < 6; j++){
       let index = Math.floor(Math.random() * str.length)
       color += str[index]
@@ -17,12 +16,33 @@ const hexaColor = () => {
   return hexArray
 }
 
-const HexaColor = () => (
-  hexaColor().map(
-    (hex) => <div key={hex} style={{backgroundColor: `${hex}`, fontSize: '25px', textAlign: 'center', padding: '20px', color: 'white'}}>{hex}</div>
-    )
+const hexData = hexaColor()
+
+const NumberTile = ({props}) => {
+  return (
+    <div style={{backgroundColor: `${props}`, color: 'white', width: '150px', height: '150px'}}>
+      <h2 style={{lineHeight: '150px'}}>{props}</h2>
+    </div>
   )
+}
+
+const NumberTiles = ({data}) => {
+  const tileList = data.map((tile) => (
+    <NumberTile key={tile} props={tile}/>
+  ))
+  return tileList
+}
+
+// App component
+const App = () => (
+<div style={{textAlign: 'center'}}>
+  <h1>30 Days of React</h1>
+  <h2>Hexadecimal Colors</h2>
+  <div style={{display: 'grid', gridTemplateColumns: 'repeat(8, 150px)', margin: 'auto', padding: '10px', width: '65%'}}>
+    <NumberTiles data={hexData}/>
+  </div>
+</div>
+)
 
 const rootElement = document.getElementById('root')
-// we render the App component using the ReactDOM package
-ReactDOM.render(<HexaColor />, rootElement)
+ReactDOM.render(<App />, rootElement)
