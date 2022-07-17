3caf995c075176a388b3308fe5c883f5f99b2afb
Vincent H <hoangvincent69@gmail.com>
2022-07-17 02:13:42 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,81 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import Main from './components/main/Main.js'
-import Header from './components/header/Header.js'
-import Footer from './components/footer/Footer.js'
 
-class App extends React.Component {
-    state = {
-      loggedIn: false,
-      techs: ['HTML', 'CSS', 'JS'],
-      message: 'Click show time or Greet people to change me',
-    }
-    handleLogin = () => {
-      this.setState({
-        loggedIn: !this.state.loggedIn,
-      })
-    }
-    showDate = (time) => {
-      const months = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-      ]
-  
-      const month = months[time.getMonth()].slice(0, 3)
-      const year = time.getFullYear()
-      const date = time.getDate()
-      return `${month} ${date}, ${year}`
-    }
-    handleTime = () => {
-      let message = this.showDate(new Date())
-      this.setState({ message })
-    }
-    greetPeople = () => {
-      let message = 'Welcome to 30 Days Of React Challenge, 2020'
-      this.setState({ message })
-    }
-  
-    render() {
-      const data = {
-        welcome: '30 Days Of React',
-        title: 'Getting Started React',
-        subtitle: 'JavaScript Library',
-        author: {
-          firstName: 'Asabeneh',
-          lastName: 'Yetayeh',
-        },
-        date: 'Oct 9, 2020',
-      }
-      const techs = ['HTML', 'CSS', 'JavaScript']
-  
-      return (
-        <div className='app'>
-          {this.state.backgroundColor}
-          <Header data={data} />
-  
-          <Main
-            techs={techs}
-            handleTime={this.handleTime}
-            greetPeople={this.greetPeople}
-            loggedIn={this.state.loggedIn}
-            handleLogin={this.handleLogin}
-            message={this.state.message}
-          />
-  
-          <Footer date={new Date()} />
-        </div>
-      )
+class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log('I am  the constructor and  I will be the first to run.')
+    this.state = {
+      firstName: 'John',
+      day: 1,
     }
   }
 
-  export default App
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(nextProps, nextState)
+    console.log(nextState.day)
+    if (nextState.day > 31) {
+      return false
+    } else {
+      return true
+    }
+  }
+  // the doChallenge increment the day by one
+  doChallenge = () => {
+    this.setState({
+      day: this.state.day + 1,
+    })
+  }
+  render() {
+    return (
+      <div className='App'>
+        <h1>React Component Life Cycle</h1>
+        <button onClick={this.doChallenge}>Do Challenge</button>
+        <p>Challenge: Day {this.state.day}</p>
+        {this.state.congratulate && <h2>{this.state.congratulate}</h2>}
+      </div>
+    )
+  }
+}
+
+export default App
